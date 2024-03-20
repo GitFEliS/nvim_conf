59f077f20044ac0da1a56200d7787f6d948d3b51
GitFEliS <dmitrii.nuzdin@gmail.com>
2024-03-20 20:17:21 +0300

    added some plugins and fix something
    	modified:   .luarc.json
    	modified:   lazy-lock.json
    	modified:   lua/plugins/completion.lua
    	modified:   lua/plugins/git.lua
    	modified:   lua/plugins/none-ls.lua
    	new file:   lua/plugins/terminal.lua
    	new file:   lua/plugins/whichkey.lua

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -14,6 +14,55 @@ return {
 					diffview = require("diffview"),
 				},
 			})
+		vim.keymap.set("n", "<leader>ng", ":Neogit<Enter>", {})
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+
+		config = function()
+			require("gitsigns").setup({
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
+
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+
+					-- Navigation
+					map("n", "]c", function()
+						if vim.wo.diff then
+							return "]c"
+						end
+						vim.schedule(function()
+							gs.next_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
+
+					map("n", "[c", function()
+						if vim.wo.diff then
+							return "[c"
+						end
+						vim.schedule(function()
+							gs.prev_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
+
+					map("n", "<leader>hs", gs.stage_hunk)
+					map("n", "<leader>hr", gs.reset_hunk)
+					map("n", "<leader>hp", gs.preview_hunk)
+					map("n", "<leader>hd", gs.diffthis)
+				end,
+
+				current_line_blame = true,
+				current_line_blame_opts = {
+					delay = 5000,
+				},
+			})
 		end,
 	},
 }
---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		"voldikss/vim-floaterm",
+		config = function()
+			-- require("vim-floaterm")
+			vim.keymap.set("n", "<F1>", ":FloatermToggle<CR>")
+		end,
+	},
+}
