c5fb2b10e824a686ebc3b2e78e0c5163fac0d870
GitFelis <dmitrii.nuzdin@gmail.com>
2024-12-04 01:19:32 +0300

    added some mini plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,67 +1,71 @@
 return {
-  { -- Collection of various small independent plugins/modules
-    "echasnovski/mini.nvim",
-    config = function()
-      require("mini.ai").setup({ n_lines = 500 })
+	{ -- Collection of various small independent plugins/modules
+		"echasnovski/mini.nvim",
+		config = function()
+			require("mini.ai").setup({ n_lines = 500 })
 
-      require("mini.surround").setup()
+			require("mini.surround").setup()
 
-      require("mini.basics").setup()
+			require("mini.basics").setup()
 
-      require("mini.bracketed").setup()
+			require("mini.bracketed").setup()
 
-      local miniclue = require("mini.clue")
-      miniclue.setup({
-        triggers = {
-          -- Leader triggers
-          { mode = "n", keys = "<Leader>" },
-          { mode = "x", keys = "<Leader>" },
+			local miniclue = require("mini.clue")
+			miniclue.setup({
+				triggers = {
+					-- Leader triggers
+					{ mode = "n", keys = "<Leader>" },
+					{ mode = "x", keys = "<Leader>" },
 
-          -- Built-in completion
-          { mode = "i", keys = "<C-x>" },
+					-- Built-in completion
+					{ mode = "i", keys = "<C-x>" },
 
-          -- `g` key
-          { mode = "n", keys = "g" },
-          { mode = "x", keys = "g" },
+					-- `g` key
+					{ mode = "n", keys = "g" },
+					{ mode = "x", keys = "g" },
 
-          -- Marks
-          { mode = "n", keys = "'" },
-          { mode = "n", keys = "`" },
-          { mode = "x", keys = "'" },
-          { mode = "x", keys = "`" },
+					-- Marks
+					{ mode = "n", keys = "'" },
+					{ mode = "n", keys = "`" },
+					{ mode = "x", keys = "'" },
+					{ mode = "x", keys = "`" },
 
-          -- Registers
-          { mode = "n", keys = '"' },
-          { mode = "x", keys = '"' },
-          { mode = "i", keys = "<C-r>" },
-          { mode = "c", keys = "<C-r>" },
+					-- Registers
+					{ mode = "n", keys = '"' },
+					{ mode = "x", keys = '"' },
+					{ mode = "i", keys = "<C-r>" },
+					{ mode = "c", keys = "<C-r>" },
 
-          -- Window commands
-          { mode = "n", keys = "<C-w>" },
+					-- Window commands
+					{ mode = "n", keys = "<C-w>" },
 
-          -- `z` key
-          { mode = "n", keys = "z" },
-          { mode = "x", keys = "z" },
-        },
+					-- `z` key
+					{ mode = "n", keys = "z" },
+					{ mode = "x", keys = "z" },
+				},
 
-        clues = {
-          -- Enhance this by adding descriptions for <Leader> mapping groups
-          miniclue.gen_clues.builtin_completion(),
-          miniclue.gen_clues.g(),
-          miniclue.gen_clues.marks(),
-          miniclue.gen_clues.registers(),
-          miniclue.gen_clues.windows(),
-          miniclue.gen_clues.z(),
-        },
-      })
+				clues = {
+					-- Enhance this by adding descriptions for <Leader> mapping groups
+					miniclue.gen_clues.builtin_completion(),
+					miniclue.gen_clues.g(),
+					miniclue.gen_clues.marks(),
+					miniclue.gen_clues.registers(),
+					miniclue.gen_clues.windows(),
+					miniclue.gen_clues.z(),
+				},
+			})
 
-      require("mini.comment").setup()
+			require("mini.comment").setup()
 
-      require("mini.indentscope").setup({ draw = { delay = 0 } })
+			require("mini.indentscope").setup({ draw = { delay = 0 } })
 
-      require("mini.pairs").setup()
+			require("mini.pairs").setup()
 
-      require("mini.notify").setup()
-    end,
-  },
+			require("mini.notify").setup()
+
+			require("mini.animate").setup()
+
+			require("mini.move").setup()
+		end,
+	},
 }
